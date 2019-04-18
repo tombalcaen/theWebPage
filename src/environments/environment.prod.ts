3b8af01d65e605edf1d1f6a1ef39e62a39f913da
tom balcaen <tom.balcaen88@gmail.com>
2019-04-18 11:32:49 +0200

    deleted heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   SENDGRID_API_KEY: "",
-  // connection_uri: 'https://flockconsulting.herokuapp.com/',
-  connectin_uri: 'ec2-35-180-113-244.eu-west-3.compute.amazonaws.com'
-  // connection_uri: 'http://ahodos.com.s3-website.eu-west-3.amazonaws.com'
+  connection_uri: 'ec2-52-47-149-189.eu-west-3.compute.amazonaws.com'  
 };
+
+
+// connection_uri: 'https://flockconsulting.herokuapp.com/',
