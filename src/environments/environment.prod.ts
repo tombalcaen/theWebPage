6735a5715a69a7491c2d42849bac840d2032f708
tom balcaen <tom.balcaen88@gmail.com>
2019-04-18 11:49:34 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   SENDGRID_API_KEY: "",
-  connection_uri: 'ec2-52-47-149-189.eu-west-3.compute.amazonaws.com'  
+  connection_uri: 'ec2-52-47-149-189.eu-west-3.compute.amazonaws.com/'
 };
 
 
